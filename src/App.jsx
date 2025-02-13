1b2157cddc94058b5199353e8c0c255f0b13ef02
artyomgaran <artyom.garan@mail.ru>
2025-02-13 11:11:01 +0300

    without JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,40 @@
+import React from 'react';
 import logo from './assets/react.svg';
 import './App.css';
 
-
 export const App = () => {
-	const currentYear = new Date().getFullYear(); // Декларативный стиль (описываем, что хотим получить)
-	return (
+    const currentYear = new Date().getFullYear(); // Декларативный стиль
 
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload 2.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				{/* Декларативный стиль — описываем, что должно быть в разметке */}
-				<footer>
-					<p>{currentYear}</p>
-				</footer>
-			</header>
-
-
-		</div>
-	);
+    return React.createElement(
+        "div",
+        { className: "App" },
+        React.createElement(
+            "header",
+            { className: "App-header" },
+            React.createElement("img", { src: logo, className: "App-logo", alt: "logo" }),
+            React.createElement(
+                "p",
+                null,
+                "Edit ",
+                React.createElement("code", null, "src/App.js"),
+                " and save to reload 2."
+            ),
+            React.createElement(
+                "a",
+                {
+                    className: "App-link",
+                    href: "https://reactjs.org",
+                    target: "_blank",
+                    rel: "noopener noreferrer"
+                },
+                "Learn React"
+            ),
+			// Добавляем footer с текущим годом
+			React.createElement(
+				"footer",
+				null,
+				React.createElement("p", null, `Текущий год: ${currentYear}`)
+			)
+        )
+    );
 };
